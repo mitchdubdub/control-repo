a02f448ca2a37dcc487b64cb0ccc15536a48ecd6
Mitch Wright <mitchdubdub@users.noreply.github.com>
2020-08-28 16:04:27 -0500

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -5,8 +5,11 @@ class profile::r10k {
   class {'r10k::webhook::config':
      use_mcollective => false,
      enable_ssl      => false,
+     protected       => false,
+     
   }
-  class {'r10k::webhook::params':
+  git_webhook { 'web_post_receive_webhook' :
+    ensure       => present,
   }
 }
   
