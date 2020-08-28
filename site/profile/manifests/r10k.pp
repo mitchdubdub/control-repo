025648eb5e373e158701518d1414418ef8da5322
Mitch Wright <mitchdubdub@users.noreply.github.com>
2020-08-28 16:15:21 -0500

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,15 +1,12 @@
 class profile::r10k {
-  class {'r10k':
+  class { 'r10k':
     remote => 'https://github.com/mitchdubdub/control-repo',
   }
-  class {'r10k::webhook::config':
+  class { 'r10k::webhook::config':
      use_mcollective => false,
      enable_ssl      => false,
      protected       => false,
      
-  }
-  git_webhook { 'web_post_receive_webhook' :
-    ensure       => present,
   }
 }
   
