9ed387e0bf7e6243458484bb64e93d348d2fa57e
Mitch Wright <mitchell.wright@walmart.com>
2020-09-01 10:04:30 -0500

    moved the pupetdb profile

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -3,10 +3,8 @@
 #
 #
 class profile::puppetdb {
-  node 'master' {
-    # Configure puppetdb and its underlying database
-    class { 'puppetdb': }
-    # Configure the Puppet master to use puppetdb
-    class { 'puppetdb::master::config': }
-  }
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
 }
